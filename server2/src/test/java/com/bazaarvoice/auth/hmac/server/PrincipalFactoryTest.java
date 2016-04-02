07667d0704fc819a88869249dd9faca7e646582c
Carlos Macasaet <github@carlos-m.net>
2016-04-01 23:45:11 -0700

    #49 - Jersey 2: content is not included in signature

--- server2/src/test/java/com/bazaarvoice/auth/hmac/server/PrincipalFactoryTest.java
+++ server2/src/test/java/com/bazaarvoice/auth/hmac/server/PrincipalFactoryTest.java
@@ -1,12 +1,22 @@
 package com.bazaarvoice.auth.hmac.server;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.apache.commons.io.IOUtils.copy;
 import static org.junit.Assert.fail;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -15,12 +25,15 @@ import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.SecurityContext;
 
+import org.glassfish.jersey.internal.PropertiesDelegate;
 import org.glassfish.jersey.internal.util.collection.ImmutableMultivaluedMap;
 import org.glassfish.jersey.server.ContainerRequest;
 import org.glassfish.jersey.server.ExtendedUriInfo;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
 import com.bazaarvoice.auth.hmac.common.Credentials;
@@ -117,6 +130,89 @@ public class PrincipalFactoryTest {
 
         // then
         assertEquals("principal", result);
+    }
+
+    @Test
+    public final void verifyProvideAppliesContentToSignature() throws URISyntaxException, UnsupportedEncodingException {
+        // given
+        final MultivaluedMap<String, String> parameterMap = new MultivaluedHashMap<String, String>();
+        parameterMap.putSingle("apiKey", "validApiKey");
+
+        final URI uri = new URI("https://api.example.com/path/to/resource?apiKey=validApiKey");
+        final ExtendedUriInfo uriInfo = mock(ExtendedUriInfo.class);
+        given(uriInfo.getQueryParameters()).willReturn(parameterMap);
+        given(uriInfo.getRequestUri()).willReturn(uri);
+
+        given(request.getUriInfo()).willReturn(uriInfo);
+        given(request.getHeaderString("X-Auth-Version")).willReturn("1");
+        given(request.getHeaderString("X-Auth-Signature")).willReturn("validSignature");
+        given(request.getHeaderString("X-Auth-Timestamp")).willReturn("two seconds ago");
+        given(request.getMethod()).willReturn("PUT");
+        given(request.hasEntity()).willReturn(true);
+        given(request.getEntityStream()).willReturn(new ByteArrayInputStream("content".getBytes("UTF-8")));
+
+        given(authenticator.authenticate(any(Credentials.class))).willReturn("principal");
+
+        // when
+        final String result = factory.provide();
+
+        // then
+        assertEquals("principal", result);
+        final ArgumentCaptor<Credentials> credentialsCaptor = ArgumentCaptor.forClass(Credentials.class);
+        verify(authenticator, only()).authenticate(credentialsCaptor.capture());
+        final Credentials credentials = credentialsCaptor.getValue();
+        assertEquals("validApiKey", credentials.getApiKey());
+        assertEquals("two seconds ago", credentials.getTimestamp());
+        assertEquals("PUT", credentials.getMethod());
+        assertEquals("/path/to/resource?apiKey=validApiKey", credentials.getPath());
+        assertEquals("content", new String(credentials.getContent(), "UTF-8"));
+    }
+
+    @Test
+    public final void verifyProvidePassesContentDownstream() throws URISyntaxException, IOException {
+        // given
+        final String content = "content";
+
+        final SecurityContext securityContext = mock(SecurityContext.class);
+        final PropertiesDelegate propertiesDelegate = mock(PropertiesDelegate.class);
+        final MultivaluedMap<String, String> parameterMap = new MultivaluedHashMap<String, String>();
+        parameterMap.putSingle("apiKey", "validApiKey");
+
+        final URI uri = new URI("https://api.example.com/path/to/resource?apiKey=validApiKey");
+        final ExtendedUriInfo uriInfo = mock(ExtendedUriInfo.class);
+        given(uriInfo.getQueryParameters()).willReturn(parameterMap);
+        given(uriInfo.getRequestUri()).willReturn(uri);
+
+        final ContainerRequest containerRequest = new ContainerRequest(new URI("https://api.example.com"),
+                new URI("https://api.example.com/path/to/resource?apiKey=validApiKey"), "POST", securityContext,
+                propertiesDelegate);
+        request = spy(containerRequest);
+        request.setEntityStream(new ByteArrayInputStream(content.getBytes("UTF-8")));
+        request.header("X-Auth-Version", "1");
+        request.header("X-Auth-Signature", "validSignature");
+        request.header("X-Auth-Timestamp", "two seconds ago");
+        given(request.getMethod()).willReturn("POST");
+        given(request.getUriInfo()).willReturn(uriInfo);
+
+        given(requestProvider.get()).willReturn(request);
+
+        given(authenticator.authenticate(any(Credentials.class))).willReturn("principal");
+
+        // when
+        final String result = factory.provide();
+
+        // then
+        assertEquals("principal", result);
+        assertTrue(request.hasEntity());
+        final InputStream entityStream = request.getEntityStream();
+        try {
+            final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            copy(entityStream, outputStream);
+
+            assertEquals(content, new String(outputStream.toByteArray(), "UTF-8"));
+        } finally {
+            entityStream.close();
+        }
     }
 
 }
